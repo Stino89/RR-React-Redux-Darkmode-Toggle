b8328e22d5580ecc3fbaee8d79d033ac291b71c4
Faustino Ruiz <ftr1989@gmail.com>
2024-04-18 20:00:52 -0700

    class

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import themeReducer from './features/themeSlice'
+//import { logger } from './features/middleware'
+export const store = configureStore({
+    reducer: {
+        theme: themeReducer
+    },
+    //middleware: [logger]
+})
